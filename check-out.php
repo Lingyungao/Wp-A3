0b7f7814c30f28268b2622ab847509e7db8ca257
SpZanG <SpZ@192-168-1-4.tpgi.com.au>
2015-10-06 00:36:30 +1100

    finish some part

--- check-out.php
+++ check-out.php
@@ -4,94 +4,71 @@
     <meta charset="UTF-8">
  </head>
  <body>
+    <!-- show the ditile of the tickets -->
  	<table>
+        <tr border="1" style="width:100%">
+            <td>
  	<?php
  		echo "{$_POST['username']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Silverado";
  		echo "<br>";
  		echo "{$_POST['email']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Inside Out";
  		echo "<br>";
- 		echo "{$_POST['phonenumber']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['movieday']},&nbsp;{$_POST['movietime']}"
+ 		echo "{$_POST['phonenumber']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['movieday']},&nbsp;{$_POST['movietime']}";
  		echo "<br>";
  		echo "<br>";
- 		if ($_POST['samovieseat']!="0")
+ 		         if ($_POST['samovieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Adult seats</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['samovieseat']}</td>
-                    <td>{$_POST['samovieseatnum']}</td>
-                    <td>{$_POST['saprice']}</td>
-                    </tr>";
+                    "{$_POST['samovieseat']}&nbsp;*&nbsp;Adult seats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['saprice']}";
 
                 if ($_POST['spmovieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Concession seats</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['spmovieseat']}</td>
-                    <td>{$_POST['spmovieseatnum']}</td>
-                    <td>{$_POST['spprice']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['spmovieseat']}&nbsp;*&nbsp;Concession seats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['spprice']}";
 
                 if ($_POST['scmovieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Child seats</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['scmovieseat']}</td>
-                    <td>{$_POST['scmovieseatnum']}</td>
-                    <td>{$_POST['scprice']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['scmovieseat']}&nbsp;*&nbsp;Child seats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['scprice']}";
 
                 if ($_POST['famovieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>First Class Adult seats</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['famovieseat']}</td>
-                    <td>{$_POST['famovieseatnum']}</td>
-                    <td>{$_POST['faprice']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['famovieseat']}&nbsp;*&nbsp;First Class Adult seats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['faprice']}";
 
                 if ($_POST['fcmovieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>First Class Child seats</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['fcmovieseat']}</td>
-                    <td>{$_POST['fcmovieseatnum']}</td>
-                    <td>{$_POST['fcprice']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['fcmovieseat']}&nbsp;*&nbsp;First Class Child seats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['fcprice']}";
+
                 if ($_POST['b1movieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Beanbag seats (single person)</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['b1movieseat']}</td>
-                    <td>{$_POST['b1movieseatnum']}</td>
-                    <td>{$_POST['b1price']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['b1movieseat']}&nbsp;*&nbsp;Beanbag seats (single person)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['b1price']}";
 
                 if ($_POST['b2movieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Beanbag seats (Up to 2 people)</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['b2movieseat']}</td>
-                    <td>{$_POST['b2movieseatnum']}</td>
-                    <td>{$_POST['b2price']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['b2movieseat']}&nbsp;*&nbsp;Beanbag seats (Up to 2 people)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['b2price']}";
 
                 if ($_POST['b3movieseat']!="0")
                     echo
-                    "<tr class='table-background-second'>
-                    <td>Beanbag seats (Up to 3 children)</td>
-                    <td>$12.00</td>
-                    <td>{$_POST['b3movieseat']}</td>
-                    <td>{$_POST['b3movieseatnum']}</td>
-                    <td>{$_POST['b3price']}</td>
-                    </tr>";
+                    "<br>
+                    {$_POST['b3movieseat']}&nbsp;*&nbsp;Beanbag seats (Up to 3 children)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$_POST['b3price']}";
                 ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Silverado
+                <br>
+                Inside Out
+                <?php echo "{$_POST['movieday']},&nbsp;{$_POST['movietime']}";?>
+                <br>
+                <br>
+                <br>
+                <br>
+            </td>
+        </tr>
+
     </table>
 </body>
 </html>
